9e2e0f5a77242d5173cd10be9797c28db48867a3
IsaacHolley <93732136+IsaacHolley@users.noreply.github.com>
2023-02-16 12:56:52 +0000

    Add files via upload

---
+++ Firebase/js/firebase.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyCskiJE1Gphl5OpwtKnsnc7f2Rt1BjpXzE",
+  authDomain: "fir-top5isaac.firebaseapp.com",
+  projectId: "fir-top5isaac",
+  storageBucket: "fir-top5isaac.appspot.com",
+  messagingSenderId: "699744879010",
+  appId: "1:699744879010:web:3a656986b6fc5ceea2b2fb",
+  measurementId: "G-VB7CC8HJE1"
+});
+
+var db = firebase.firestore();
